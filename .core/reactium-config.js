7a695a3f91fd04a848ffc65f74949514f0c5f53b
Cam Tullos <>
2021-09-21 12:12:41 -1000

    Added reactium-config file.

---
+++ .core/reactium-config.js
@@ -0,0 +1,92 @@
+const fs = require('fs');
+const path = require('path');
+const globby = require('./globby-patch');
+const rootPath = path.resolve(__dirname, '..');
+const gulpConfig = require('./gulp.config');
+
+const version = '0.0.1';
+
+/**
+ * Use liberally for additional core configuration.
+ * @type {Object}
+ */
+module.exports = {
+    version,
+    semver: '^0.0.1',
+    update: {
+        package: {
+            dependencies: {
+                remove: [],
+            },
+            devDependencies: {
+                remove: [],
+            },
+            scripts: {
+                add: {
+                    android: 'arcli start -t android',
+                    ios: 'arcli start -t ios',
+                },
+                remove: [],
+            },
+        },
+        files: {
+            add: [
+                {
+                    overwrite: true,
+                    version: '>=0.0.1',
+                    destination: '/.eslintrc',
+                    source: '/tmp/.eslintrc',
+                },
+                {
+                    overwrite: true,
+                    version: '>=0.0.1',
+                    destination: '/.prettierrc.js',
+                    source: '/tmp/.prettierrc.js',
+                },
+                {
+                    overwrite: true,
+                    version: '>=0.0.1',
+                    destination: '/gulpfile.js',
+                    source: '/tmp/gulpfile.js',
+                },
+                {
+                    overwrite: true,
+                    version: '>=0.0.1',
+                    destination: '/.prettierignore',
+                    source: '/tmp/.prettierignore',
+                },
+                {
+                    overwrite: false,
+                    version: '>=0.0.1',
+                    destination: '/.flowconfig',
+                    source: '/tmp/.flowconfig',
+                },
+                {
+                    overwrite: false,
+                    version: '>=0.0.1',
+                    destination: '/.huskyrc',
+                    source: '/tmp/.huskyrc',
+                },
+                {
+                    overwrite: true,
+                    version: '>=0.0.1',
+                    destination: '/index.js',
+                    source: '/tmp/index.js',
+                },
+                {
+                    overwrite: true,
+                    version: '>=0.0.1',
+                    destination: '/babel.config.js',
+                    source: '/tmp/babel.config.js',
+                },
+                {
+                    overwrite: true,
+                    version: '>=0.0.1',
+                    destination: '/metro.config.js',
+                    source: '/tmp/metro.config.js',
+                },
+            ],
+            remove: [],
+        },
+    },
+};
