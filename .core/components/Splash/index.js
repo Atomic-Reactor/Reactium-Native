88bacefb01b18c3114febf085d76bf38124c29ff
Cam Tullos <>
2022-01-26 13:53:18 -1000

    Updated default splash screen component to be hookable: 'splash-screen'.

--- .core/components/Splash/index.js
+++ .core/components/Splash/index.js
@@ -1,21 +1,42 @@
 import React, { useEffect } from 'react';
 import SplashScreen from 'react-native-splash-screen';
+import Reactium, { useSyncState } from 'reactium-core/sdk';
 import AnimatedSplash from 'react-native-animated-splash-screen';
 
-export default ({ children, isLoaded = false }) => {
+const Component = ({ children, ...props }) => {
+    const state = useSyncState({
+        ...props,
+    });
+
     useEffect(() => {
         SplashScreen.hide();
+
+        const newProps = { ...state.get() };
+
+        Reactium.Hook.runSync('splash-screen', newProps);
+
+        delete newProps.isLoaded;
+
+        state.set(newProps);
     }, []);
 
-    return (
-        <AnimatedSplash
-            translucent
-            logoWidth={96}
-            logoHeight={96}
-            isLoaded={isLoaded}
-            backgroundColor={'#FFFFFF'}
-            logoImage={require('./assets/logo.png')}>
-            {children}
-        </AnimatedSplash>
-    );
+    useEffect(() => {
+        const isLoaded = state.get('isLoaded');
+        if (isLoaded !== props.isLoaded) {
+            state.set('isLoaded', props.isLoaded);
+        }
+    }, [props.isLoaded]);
+
+    return <AnimatedSplash {...state.get()} children={children} />;
 };
+
+Component.defaultProps = {
+    backgroundColor: '#FFFFFF',
+    isLoaded: false,
+    logoWidth: 96,
+    logoHeight: 96,
+    logoImage: require('./assets/logo.png'),
+    translucent: true,
+};
+
+export default Component;
