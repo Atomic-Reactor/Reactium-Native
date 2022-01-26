e962a448151931a0af3acbb3c532aee879bc0221
Cam Tullos <>
2022-01-26 09:46:42 -1000

    Encapsulate SVG reactium-metro.js so it's not running wild in the file scope.

--- .core/components/SVG/reactium-metro.js
+++ .core/components/SVG/reactium-metro.js
@@ -1,24 +1,26 @@
-const chalk = require('chalk');
-const op = require('object-path');
+(() => {
+    const chalk = require('chalk');
+    const op = require('object-path');
+    
+    ReactiumMetro.Hook.registerSync('metro-config', config => {
+        console.log(
+            `\t${chalk.cyan('sync-hook')}:${chalk.magenta(
+                'metro-config',
+            )} - ./.core/components/SVG/reactium-metro.js : line 4`,
+        );
 
-ReactiumMetro.Hook.registerSync('metro-config', config => {
-    console.log(
-        `\t${chalk.cyan('sync-hook')}:${chalk.magenta(
-            'metro-config',
-        )} - ./.core/components/SVG/reactium-metro.js : line 4`,
-    );
+        const assetExts = op.get(config, 'resolver.assetExts', []);
 
-    const assetExts = op.get(config, 'resolver.assetExts', []);
+        op.set(
+            config,
+            'resolver.assetExts',
+            assetExts.filter(ext => ext !== 'svg'),
+        );
 
-    op.set(
-        config,
-        'resolver.assetExts',
-        assetExts.filter(ext => ext !== 'svg'),
-    );
-
-    op.set(
-        config,
-        'transformer.babelTransformerPath',
-        require.resolve('react-native-svg-transformer'),
-    );
-});
+        op.set(
+            config,
+            'transformer.babelTransformerPath',
+            require.resolve('react-native-svg-transformer'),
+        );
+    });
+})();
